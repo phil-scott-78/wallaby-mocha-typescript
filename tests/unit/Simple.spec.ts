e9c1550c4642b898131abc78dc5ffb24b5b5e008
Phil Scott <phil.scott@gmail.com>
2018-07-22 17:37:51 -0400

    adding simple test

---
+++ tests/unit/Simple.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+describe('simpliest', () => {
+  it('can do the do', () => {
+    expect(1).to.equal(1);
+  });
+});
